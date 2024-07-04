d6d94d1cdbb181c52b45bfd063ae85cb773044bd
Mehr_code <116884284+Mehr-code@users.noreply.github.com>
2024-07-04 23:58:59 +0330

    Update 82519.js

--- SRC/82519.js
+++ SRC/82519.js
@@ -9,8 +9,10 @@ const ERROR_TYPE = {
 };
 let numbers = [4, 6, 10, 23, 0, 24, 30, 2];
 
+// فضایی که قراره اعداد در قالب اِسپن قرار بگیره
 const numbersContainer = document.getElementById("numbers-container");
 
+// تابع ای برای ساختن لیست اعداد در تگ اِسپن
 function creatSpan(numbers) {
   numbers.forEach((el) => {
     const span = document.createElement("span");
@@ -19,13 +21,24 @@ function creatSpan(numbers) {
   });
 }
 
-creatSpan(numbers);
-
-const btn = document.getElementById("submit-btn");
-
-function func1() {
-  const item = document.getElementById("item-input").value;
-  const count = document.getElementById("count-input").value;
+// تابع برای اینکه بفهمم جهت جا به جایی کدام سمت است
+function whichDirection() {
+  // انتخاب تمام رادیو باتن ها
+  const radios = document.getElementsByName("type");
+  let selectedValue;
+  // گرفتن مقدار رادیو باتن کلیک شده
+  for (const radio of radios) {
+    if (radio.checked) {
+      selectedValue = radio.value;
+      break;
+    }
+  }
+  if (selectedValue === "DOWN") {
+    return true;
+  }
+}
+// تابع جا به جایی اعداد
+function swappingNumbers(item, count) {
   let index = numbers.indexOf(Number(item));
   for (let i = 0; i < count; i++) {
     const temp = numbers[index + 1];
@@ -33,8 +46,34 @@ function func1() {
     numbers[index] = temp;
     index++;
   }
+}
+// لیست اعداد را بساز با آرایه ی نامبرز
+creatSpan(numbers);
+// انتخاب دکمه سابمیت
+const btn = document.getElementById("submit-btn");
+
+// تابع اصلی برای جا به جایی اعداد
+function func1() {
+  // گرفتن عدد در قالب آیتم
+  const item = document.getElementById("item-input").value;
+  // چقد جا به جا بشه
+  const count = document.getElementById("count-input").value;
+  // در کدام جهت جا به جا کنیم
+  if (whichDirection()) {
+    numbers = numbers.reverse();
+
+    // فراخواندن تابع جا به جایی
+    swappingNumbers(item, count);
+
+    numbers = numbers.reverse();
+  } else {
+    swappingNumbers(item, count);
+  }
+
+  // پاک کردن فضای اعداد برای دوباره گذاشتن اظلاعات پس از جابه جایی
   numbersContainer.innerHTML = "";
   creatSpan(numbers);
 }
 
+// هنگامی که سابمیت کلیک شد تابع اصلی جا به جایی را فرا بخوان
 btn.addEventListener("click", func1);
