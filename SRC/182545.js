98d69fa9f8d57294ffb44625cd9955030f8ba0b0
Mehr_code <116884284+Mehr-code@users.noreply.github.com>
2024-07-08 21:54:04 +0330

    Update 182545.js

--- SRC/182545.js
+++ SRC/182545.js
@@ -1,5 +1,55 @@
 function encrypt(str, n) {
-  //encrypt function
+  // تبدیل رشته به آرایه
+  let txt = str.split("");
+
+  // آرایه ای که قراره جواب ریخته شه توش
+  const result = [];
+
+  // تمامی اعداد با محور بر حسب آ سر و کار دارند
+  const pivot = "a".charCodeAt(0);
+
+  // اگر عدد بزرگ بود، فلگ تورو میشود
+  let flag = false;
+
+  // بررسی تمامی اعداد وارد شده در قالب آرایه
+  txt.forEach((element) => {
+    // اگر عنصر مورد بررسی خالی بود، همون وارد بشه
+    if (element === " ") {
+      result.push(element);
+    } else {
+      // عنصر اگر بزرگ بود باید کوچک بشه و در نهایت باز بزرگ بشود
+      if (element === element.toUpperCase()) {
+        flag = true;
+        element = element.toLowerCase();
+      }
+      // عدد عنصر در بازه ی 0 تا 25
+      let number = element.charCodeAt(0) - pivot;
+
+      // عدد را شیفت میدهیم
+      number += n;
+
+      // اگر عدد بیشتر از 26 شد باید چرخشی طور شیفت بدهد
+      while (number > 26) {
+        number -= 26;
+      }
+
+      // عدد را باز به کاراکتر تبدیل میکنیم
+      let char = String.fromCharCode(number + pivot);
+
+      // اگر عدد بزرگ بود، به بزرگ باز تبدیل میشود
+      if (flag) {
+        char = char.toUpperCase();
+      }
+      // عنصر تبدیل شده در آرایه نتیجه ریخته میشود
+      result.push(char);
+
+      // فلگ به پیش فرض خود بر میگردد
+      flag = false;
+    }
+  });
+
+  // تبدیل آرایه نهایی به رشته
+  return result.join("");
 }
 
 function decrypt(str, n) {
@@ -56,7 +106,4 @@ function decrypt(str, n) {
   return result.join("");
 }
 
-console.log(decrypt("Byffi Qilfx", 20));
-
-// export { encrypt, decrypt };
-// ByffiQilfx
+export { encrypt, decrypt };
