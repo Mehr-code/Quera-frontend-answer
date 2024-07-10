5e0721f1f7ccce49ef7b8c32be06da737b39d651
Mehr_code <116884284+Mehr-code@users.noreply.github.com>
2024-07-10 23:15:52 +0330

    Update 98447.js

--- SRC/98447.js
+++ SRC/98447.js
@@ -1,20 +1,42 @@
-let horizontalLines = [
-  [false, false, false, false, false, false, false],
-  [false, false, false, false, false, false, false],
-  [false, false, false, false, false, false, false],
-  [false, false, false, false, false, false, false],
-  [false, false, false, false, false, false, false],
-];
-let verticalLines = [
-  [false, false, false, false, false, false, false, false],
-  [false, false, false, false, false, false, false, false],
-  [false, false, false, false, false, false, false, false],
-  [false, false, false, false, false, false, false, false],
-  [false, false, false, false, false, false, false, false],
-];
+// // نمایش خط های افقی با آرایه
+// let horizontalLines = [
+//   [false, false, false, false, false, false, false],
+//   [false, false, false, false, false, false, false],
+//   [false, false, false, false, false, false, false],
+//   [false, false, false, false, false, false, false],
+//   [false, false, false, false, false, false, false],
+// ];
+
+// // نمایش خط های عمودی با آرایه
+// let verticalLines = [
+//   [false, false, false, false, false, false, false, false],
+//   [false, false, false, false, false, false, false, false],
+//   [false, false, false, false, false, false, false, false],
+//   [false, false, false, false, false, false, false, false],
+//   [false, false, false, false, false, false, false, false],
+// ];
 
 // استفاده از دام کانتت لودد جای لود که باعث سریع تر شدن کار میشود
 document.addEventListener("DOMContentLoaded", () => {
+  // نمایش خط های افقی با آرایه
+  let horizontalArray = [
+    [false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false],
+  ];
+
+  // نمایش خط های عمودی با آرایه
+  let verticalArray = [
+    [false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false],
+    [false, false, false, false, false, false, false, false],
+  ];
+
   // نوبت اولیه بازی
   let currentPlayer = "blue";
 
@@ -22,51 +44,69 @@ document.addEventListener("DOMContentLoaded", () => {
   const playerTurnElement = document.getElementById("player_turn");
   playerTurnElement.textContent = currentPlayer;
 
-  // Add event listeners to all horizontal lines
+  // انتخاب تمامی خط های افقی در قالب یک متغیر
   const horizontalLines = document.querySelectorAll(".horizontal_line");
-
+  // ... به ازای هر خط افقی
   horizontalLines.forEach((line, index) => {
+    // اگر کلیکی رخ داد تابع هندل کلیک را صدا بزن
     line.addEventListener("click", () =>
       handleLineClick(line, "horizontal", index)
     );
 
+    // اگر موس آمد روی خط تابع هندل موس اینتر را صدا بزن
     line.addEventListener("mouseenter", () =>
       handleMouseEnter(line, "horizontal", index)
     );
+
+    // اگر موس از خط رفت تابع هندل موس لیو را صدا بزن
     line.addEventListener("mouseleave", () =>
       handleMouseLeave(line, "horizontal", index)
     );
   });
 
-  // Add event listeners to all vertical lines
+  // انتخاب تمامی خط های عمودی در قالب یک متغیر
   const verticalLines = document.querySelectorAll(".vertical_line");
   verticalLines.forEach((line, index) => {
     line.addEventListener("click", () =>
       handleLineClick(line, "vertical", index)
     );
+
+    // اگر موس آمد روی خط تابع هندل موس اینتر را صدا بزن
+    line.addEventListener("mouseenter", () =>
+      handleMouseEnter(line, "horizontal", index)
+    );
+
+    // اگر موس از خط رفت تابع هندل موس لیو را صدا بزن
+    line.addEventListener("mouseleave", () =>
+      handleMouseLeave(line, "horizontal", index)
+    );
   });
 
+  // تابع لوکیشن خط بر اساس ایندکس که ردیف و شماره خط را میبابد
   function locLine(index) {
     let i = 0;
     while (index > i * 7) {
       i++;
     }
-    let gruop = i;
+    let row = i;
     index = index % 7;
-    return [index, gruop];
+    return [index, row];
   }
 
+  // این تابع خطی که کلیک شده را بررسی میکند
   function handleLineClick(line, type, index) {
-    let [num, gruop] = locLine(index);
+    // لوکیشن خط را پیدا میکند
+    let [num, row] = locLine(index);
+    // اگر خط افقی باشد
     if (type === "horizontal") {
-      if (!horizontalLines[gruop][num]) {
-        line.classList.add(`${currentPlayer}`);
-        horizontalLines[gruop][num] = true;
+      if (!horizontalArray[row][num]) {
+        lineChanger(line, row, num);
       }
-    } else if (type === "vertical") {
-      if (!verticalLines[gruop][num]) {
-        line.classList.add(`${currentPlayer}`);
-        verticalLines[gruop][num] = true;
+    }
+    // اگر خط عمودی باشد
+    else if (type === "vertical") {
+      if (!verticalArray[row][num]) {
+        lineChanger(line, row, num);
       }
     }
 
@@ -77,13 +117,15 @@ document.addEventListener("DOMContentLoaded", () => {
     updateStatus();
   }
   function handleMouseEnter(line, type, index) {
-    let [num, gruop] = locLine(index);
+    let [num, row] = locLine(index);
+
     if (type === "horizontal") {
-      if (!horizontalLines[gruop][num]) {
+      console.log(horizontalArray);
+      if (!horizontalArray[row][num]) {
         line.classList.add(`${currentPlayer}`);
       }
     } else if (type === "vertical") {
-      if (!verticalLines[gruop][num]) {
+      if (!verticalArray[row][num]) {
         line.classList.add(`${currentPlayer}`);
       }
     }
@@ -92,18 +134,23 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function handleMouseLeave(line, type, index) {
-    let [num, gruop] = locLine(index);
+    let [num, row] = locLine(index);
     if (type === "horizontal") {
-      if (!horizontalLines[gruop][num]) {
+      if (!horizontalArray[row][num]) {
         line.classList.remove(`${currentPlayer}`);
       }
     } else if (type === "vertical") {
-      if (!verticalLines[gruop][num]) {
+      if (!verticalArray[row][num]) {
         line.classList.remove(`${currentPlayer}`);
       }
     }
 
     updateStatus();
+  }
+
+  function lineChanger(line, row, num) {
+    line.classList.add(`${currentPlayer}`);
+    horizontalArray[row][num] = true;
   }
   function checkForBoxCompletion(type, index) {
     // Check for box completion logic based on the line clicked (horizontal or vertical)
