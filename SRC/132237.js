2e5229e44338c9d1c3ecbdfb3bed6ccbd6576a10
Mehr_code <116884284+Mehr-code@users.noreply.github.com>
2024-07-17 22:09:29 +0330

    Update 132237.js

--- SRC/132237.js
+++ SRC/132237.js
@@ -66,47 +66,64 @@ function registerEvents() {}
 
 // تابع دکمه همه به سمت راست
 allToTheRight.addEventListener("click", () => {
+  // چک میکنیم که لیست راست چیزی داره که انتقال بدیم؟
   if (leftList == false) {
     return;
   } else {
+    // غیز فعال کردن دکمه های به چپ
     allToTheRight.classList.add("disabled");
     checkToRight.classList.add("disabled");
+    // برای محکم کاری نوشتم که دکمه های به راست حتما فعال باشه
     allToTheLeft.classList.remove("disabled");
     checkToLeft.classList.remove("disabled");
+    // از لیست آرایه چپ میدیم راست
     Array.prototype.push.apply(rightList, leftList);
+    // آرایه چپ خالی باید بشه
     leftList = [];
+    // خالی کردن باکس ها برای نمایش دادن اطلاعات جدید
     clearDom();
+    // نمایش اطلاعات جدید
     renderDom(leftList, rightList);
   }
 });
 
-// تابع دکمه چک خورده ها به سمت راست
+// تابع دکمه علامت دار ها به سمت راست
 checkToRight.addEventListener("click", () => {
-  const boxes = document.querySelectorAll(".left-side .box");
-
-  boxes.forEach(function (el) {
+  // انتخاب آیتم های موجود در باکس سمت چپ
+  const items = document.querySelectorAll(".left-side .box");
+  // به ازای هر آیتم موجود اول چک میکنیم که علامت دار هست یا نه
+  items.forEach(function (el) {
     if (el.firstElementChild.checked) {
+      // اگر علامت دار بود، آیدشو میگیریم
       const id = el.firstElementChild.id;
+      // به ازای آرایه سمت چپ اگر اون عضو در آرایه آیدی اش با آیدی ما یکی بود
       leftList.forEach(function (el) {
         if (el.id === id) {
+          // اون آیتم میره توی باکس راست
           rightList.push(el);
+          // ایندکس اش هم میگیریم تا در آرایه هم انتقال صورت بگیره
           let index = leftList.indexOf(el);
           leftList.splice(index, 1);
         }
       });
     }
   });
+  // اینجا چک میکنیم که اگر تمامی آیتم های موجود در سمت چپ علامت دار شد
+  // باید دکمه های انتقال به راست غیر فعال بشه
   if (leftList == false) {
     allToTheLeft.classList.remove("disabled");
     checkToLeft.classList.remove("disabled");
     allToTheRight.classList.add("disabled");
     checkToRight.classList.add("disabled");
   }
+
+  // خالی کردن باکس ها برای نمایش دادن اطلاعات جدید
   clearDom();
+  // نمایش اطلاعات جدید
   renderDom(leftList, rightList);
 });
 
-// تابع دکمه چک خورده ها به سمت راست
+// تابع دکمه چک خورده ها به سمت چپ
 checkToLeft.addEventListener("click", () => {
   const boxes = document.querySelectorAll(".right-side .box");
 
